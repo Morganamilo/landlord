50ac3f32fbff29620ea7be2aecfa11f3127e530e
Nathan Masi <nathanmasi8@hotmail.com>
2017-11-27 20:53:31 +0000

    Deleted unwanted old directory in __old

--- __old/old/game.h
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h>
-
-void psb();
-void renmap(int part); // render map
-void play(); // start off play method
-bool cfte;
-void settings(); // settings that can easily be changed from main menu
